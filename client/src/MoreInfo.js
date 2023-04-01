4a25fbc77218a0b643af55dea074b8f2cdd42d20
Pavan17-cpu <110300098+Pavan17-cpu@users.noreply.github.com>
2023-04-01 16:40:43 +0530

    Update MoreInfo.js

--- client/src/MoreInfo.js
+++ client/src/MoreInfo.js
@@ -12,21 +12,8 @@ function MoreInfo() {
       <h2 >ABSTRACT</h2>
       <font size="+1">
       <p>
-          A system that will manage the activities of a car rental was the primary objective of this 
-          research study. With the implementation of the proposed car rental management system with 
-          scheduling algorithm, the transaction of the business will become easy and reliable. The development 
-          system will enable the users to effectively manage the transaction, scheduling and inventory of cars of 
-          the car rental business. The researchers used Extreme Programming Methodology since this type of 
-          software development methodology helped the researchers in the planning, designing, implementation 
-          and maintenance of the developed system. Based on the results gathered from the respondents of the 
-          study, the application gained a positive response on the criteria Speed and Graphical User Interface. It 
-          was noticeable that the respondents were satisfied on the performance of the developed system and the 
-          scheduling algorithm implemented in the system efficiently performed its desired operation. Based on 
-          the information the researchers gathered, the developed application for car rental business with the 
-          integration of scheduling algorithm was useful and appropriate solution to the identified problems by 
-          the researchers.<br/><br/>
-      The system manages the most important components of a car rental company through a modular network, which can be adapted to any country
-       or size. It connects management of a fleet, people, and business, to make the operations as efficient as possible.<br/><br/>
+      Room rentals are a vital aspect of the housing market, providing affordable living arrangements for people who may not be able to afford a mortgage or a long-term lease. They are especially important for students, travelers, and people who are relocating for work.
+<br/><br/>
       <br/>
     </p>
       </font>
@@ -35,13 +22,7 @@ function MoreInfo() {
       <h2>INTRODUCTION</h2>
       <font size="+1">
         <p>
-        In 1993, National Car Rental faced liquidation. General Motors Corporation (National's parent) took a $744 million charge against 
-      earnings related to its ownership of National Car Rental Systems. National faced liquidation, with the loss of 7,500 jobs, unless it 
-      could show a profit in the short term. National initiated a comprehensive revenue management program whose core is a suite of analytic 
-      models developed to manage capacity, pricing, and reservation. As it improved management of these functions, National dramatically
-       increased its revenue. The initial implementation in July 1993 produced immediate results and returned National Car Rental to 
-       profitability. In July 1994, National implemented a state-of-the-art revenue management system, improving revenues by $56 million 
-       in the first year. In April 1995, General Motors sold National Car Rental Systems for an estimated $1.2 billion.<br/><br/>
+        Room rentals are also beneficial for homeowners who have extra space in their homes and want to earn some extra income. By renting out a room, they can offset their housing costs and make use of underutilized space in their homes.<br/><br/>
 
        <video controls width="1000">
             <source src={logo1} type="video/webm" />
@@ -52,46 +33,23 @@ function MoreInfo() {
 
 
           <br/>  
-            As man develops consciousness about the world, it is 
-            undeniable that online transaction has become a 
-            trend in transacting business today. It helps in 
-            performing task in easy way real time. Some 
-            companies have become fully automated while 
-            others striving to setup them in automation. 
-            Computer programmers develop things like 
-            computer system that the rest of us could use.<br/><br/>
+          Additionally, room rentals can help to foster a sense of community and provide opportunities for social interaction. People who rent rooms often share common areas like kitchens and living rooms, which can lead to friendships and a sense of belonging.<br/><br/>
 
-
-The advent of new technology gave rise to easy and 
-            hassle-free interaction between and among humans. 
-            Whether it is in business, science or whatever task a 
-            person takes on the quality and speed of carrying it 
-            out are enhanced with automation at the core of this 
-            efficient. The developed system for Vista entitled 
-            “Online Car Rental Reservation System”, a transaction 
-            processing system is an example of automation.<br/><br/>
+<br/><br/>
 
             
             
 
-            Car rental business is becoming big, and competition 
-            is getting high. Considering the success of online 
-            reservation in business like airline, hotel and 
-            restaurants, adopting the strategy of online 
-            reservation in car rental business will not give a 
-            different success and acceptance.<br/><br/>
+           
+<b>Flexibility</b>: Room rentals are often more flexible than traditional leases. Renters can usually rent a room on a month-to-month basis, which allows them to move out with relatively short notice. This is especially beneficial for people who may not be sure how long they will need to stay in a particular location.<br/>
 
+<b>Affordability</b>: Room rentals are often more affordable than renting an entire apartment or house. This is especially true in expensive cities, where the cost of living can be prohibitively high. Renting a room can be a cost-effective way to live in desirable neighborhoods without breaking the bank.<br/>
 
-            A rented car is a vehicle used temporarily for a 
-            period of time with a fee. An individual who wants to 
-            rent a car must first contact the car rental company 
-            for the desired vehicle. The person has to supply 
-            some information such as rent period, the desired 
-            type of car. The rental cars are categorized into 
-            economy, compact, compact premium, premium and 
-            luxury. Customers are free to choose any car based 
-            on their budget and availability of such car at the
-            time of reservation<br/><br/>
+<b>Shared Expenses:</b> When renting a room, renters often share expenses like utilities and internet with their roommates. This can help to reduce overall living expenses and make living in a desirable location more affordable.<br/>
+
+<b>Security</b>: Renting a room in someone's home can provide an added sense of security for renters. Homeowners are often more invested in the safety and security of their property, which can make renters feel safer than they would in an apartment complex or other rental property.<br/>
+
+<b>Diversity</b>: Room rentals can provide opportunities for people from different backgrounds to live together and learn from each other. This can be especially valuable for people who are new to a city or country and want to meet new people and experience new cultures.<br/><br/>
         </p>
       </font>
 
@@ -100,35 +58,7 @@ The advent of new technology gave rise to easy and
 
       <font size="+1">
         <p>
-        Customers will be able to reserve their vehicles from anywhere in the world due to the Car Rental System. Consumers 
-      provide information to this application by filling in their personal information. When a consumer creates an account on the 
-      website, he or she can reserve a car. The proposed system is an online system that is fully integrated. It effectively and efficiently
-      automates manual procedures. Customers are aided by this automated method, which allows them to fill in the specifics 
-      according to their needs. It contains information on the sort of car they want to hire as well as the location. The goal of this 
-      system is to create a website where customers can book their automobiles and request services from anywhere in the world. 
-      There are three phases to this car rental system mentioned in the introduction.
-      <br/><br/>
-
-
-
-          The car management system with the integration of 
-          scheduling algorithm provides several modules for 
-          the administrator and users of the system. It allows 
-          the booking of cars by the customers and the 
-          business could easily schedule and manage the 
-          request of the customers. The application also offers 
-          the viewing of the available cars in a car rental 
-          business. It has a transaction page where the 
-          administrator of the system could easily check the 
-          availability of the car and other information's needed 
-          by the car rental business.
-          
-          With the car rental management system, the 
-          administrator account can easily check the inventory 
-          of the available cars and other credentials by visiting 
-          the car inventory page of the application. A mark will 
-          appear in the inventory panel for the non-available 
-          cars and those which has low inventory
+        Overall, room rentals are an essential part of the housing market, providing affordable living arrangements, income for homeowners, and opportunities for social interaction.
           <br/><br/><br/>
         </p>
       </font>
